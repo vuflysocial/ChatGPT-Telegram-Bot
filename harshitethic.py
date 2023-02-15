26e79185b0a5d321dc817c1e2a473fd57675ae04
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-02-14 23:54:01 -0800

    Update harshitethic.py

--- harshitethic.py
+++ harshitethic.py
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "6235261843:AAEMYESOvS_-JE-LoKVbCr2HLPkhWEoXjHQ"
+    tgkey = "6068865994:AAF6Zf3pWBKGkvAI6WMP0sJprZuO3oYtUgw"
 else:
     tgkey = tgenv
 print(tgenv)
@@ -35,8 +35,8 @@ running = False
 cache = None
 qcache = None
 chat_log = None
-botname = 'Harshit ethic'
-username = 'harshitethic_bot'
+botname = 'meloai'
+username = 'meloai_bot'
 # Max chat log length (A token is about 4 letters and max tokens is 2048)
 max = int(3000)
 
@@ -66,8 +66,8 @@ def start(bot, update):
         chat_log = None
         cache = None
         qcache = None
-        botname = 'Harshit Ethic'
-        username = 'harshitethic_bot'
+        botname = 'meloaic'
+        username = 'meloai_bot'
         update.message.reply_text('Hi')
         return 
     else:
@@ -101,8 +101,8 @@ def reset(bot, update):
         chat_log = None
         cache = None
         qcache = None
-        botname = 'Harshit Ethic'
-        username = 'harshitethic_bot'
+        botname = 'meloai'
+        username = 'meloai_bot'
         update.message.reply_text('Bot has been reset, send a message!')
         return 
     else:
@@ -128,8 +128,8 @@ def retry(bot, update):
         chat_log = None
         cache = None
         qcache = None
-        botname = 'Harshit Ethic'
-        username = 'harshitethic_bot'
+        botname = 'meloai'
+        username = 'meloai_bot'
         update.message.reply_text('Send a message!')
         return 
     else:
@@ -189,8 +189,8 @@ def wait(bot, update, botname, username, new):
                 cache = None
                 qcache = None
                 user = ""
-                username = 'harshitethic_bot'
-                botname = 'Harshit Ethic'
+                username = 'meloai_bot'
+                botname = 'meloai'
                 update.message.reply_text('Timer has run down, bot has been reset to defaults.')
                 running = False
     else:
